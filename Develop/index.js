49fc7f487fb41750523640017b71ae3d13ca933a
Nicole Barkley <barkleylikecharles@icloud.com>
2022-03-30 22:15:02 -0400

    questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,181 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown")
+
+const fs=require('fs')
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions  = ( )=> {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your application? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project title!');
+                return false;
+            }
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project description!');
+                return false;
+            }
+        }
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions. (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('You need to enter instructions!');
+                return false;
+            }
+        }
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project description!');
+                return false;
+            }
+        }
+      },
+      {
+        type: 'input',
+        name: 'credit',
+        message: 'Who contributed to this project? (Required)',
+        validate: creditInput => {
+            if (creditInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project description!');
+                return false;
+            }
+        }
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Provide test instructions (Required)',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project description!');
+                return false;
+            }
+        }
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+            if (linkInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project GitHub link!');
+                return false;
+            }
+        }
+      },
+      {
+        type: 'input',
+        name: 'website',
+        message: 'What is the live website link? (Required)',
+        validate: websiteInput => {
+            if (websiteInput) {
+                return true;
+            } else {
+                console.log('You need to enter a link!');
+                return false;
+            }
+        }
+      },
+      {
+        type: '',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project description!');
+                return false;
+            }
+        }
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'GNU'],
+        default: ["MIT"],
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project description!');
+                return false;
+            }
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'confirmCreateReadme',
+        message: 'Would you like to generate README?',
+        default: true,
+      }
+    ]);
+    };
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+const writeFile = data => {
+    fs.writeFile('README.md', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the README has been created 
+        } else {
+            console.log("Your README has been successfully created!")
+        }
+    })
+}; 
+// // TODO: Create a function to initialize app
+// function init() {}
+// Initialize program
+questions()
+//Get user answers
+.then(answers => {
+    return generateMarkdown(answers);
+})
+// Display data 
+.then(data => {
+    return writeFile(data);
+})
+// Catching errors
+.catch(err => {
+    console.log(err)
+})
+// // Function call to initialize app
+// init();
--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Professional README Generator Starter Code
 
 [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+
+# Work Day Scheduler
+
+## Installation
+Updated and functional code can be cloned at https://github.com/barkleylikecharles/work-day-scheduler.
+
+## Usage
+Utilize this daily calendar planner to add meetings and deadlines to your schedule in hourly segments. Updates in real time to show you which calendar items have passed, which are current and which are upcoming to help keep you on track. Local storage adds the functionality of this application. Good luck and get planning!
+
+## Credits
+Project Completed by Nicole Barkley. [GitHub Profile] (https://github.com/barkleylikecharles)
+
+Starter code for project cloned from https://github.com/coding-boot-camp/super-disco
+
+## Website
+https://barkleylikecharles.github.io/work-day-scheduler/
+
+## Screenshot
+image
+
+## License
+MIT License
+
+Copyright (c) 2022 barkleylikecharles
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
